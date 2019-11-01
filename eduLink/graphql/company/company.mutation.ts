e384be762c21c966cc50bad7f92cdac7461f2ebf
tmack <tonmanayo@hotmail.com>
2019-11-02 00:12:50 +0200

    feat(api): starting to create the CRUD graph apis

--- eduLink/graphql/company/company.mutation.ts
+++ eduLink/graphql/company/company.mutation.ts
@@ -1,8 +1,39 @@
-// import { resolver } from 'graphql-sequelize';
-// import { Company } from '../../models';
-// import to from 'await-to-js';
-
+import { resolver } from 'graphql-sequelize';
+import { Company } from '../../models';
+import to from 'await-to-js';
 
 export const Mutation = {
-
+    createCompany: resolver(Company, {
+        before: async (findOptions, { data }) => {
+            let err, company;
+            [err, company] = await to(Company.create(data) );
+            if (err) {
+                throw err;
+            }
+            findOptions.where = { id: company.id };
+            return findOptions;
+        },
+        after: (company) => {
+            // company.login = true;
+            return company;
+        }
+    }),
+    deleteCompany: resolver(Company, {
+        before: async (findOptions, { id }) => {
+            findOptions.where = { id };
+            return findOptions;
+        },
+        after: async (company) => {
+            if (company && company.id) {
+                let err, _cpny;
+                [err, _cpny] = await to(Company.destroy({ where: { id: company.id } }) );
+                if (err) {
+                    throw err;
+                }
+                console.log(_cpny);
+                return company;
+            }
+            throw "Company does not exist";
+        }
+    }),
 };
--- eduLink/graphql/company/company.query.ts
+++ eduLink/graphql/company/company.query.ts
@@ -1,6 +1,27 @@
 import { resolver } from 'graphql-sequelize';
-import { Company } from '../../models';
+import {Company} from '../../models';
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 export const Query = {
     getCompany: resolver(Company),
+    getCompanies: resolver(Company, {
+        before: async (findOptions, args) => {
+            // filter the results
+            if (args.query) {
+                findOptions.where = {
+                    name: {
+                        [Op.iLike]: `%${args.query}%`
+                    }
+                };
+            }
+            // order by name column
+            findOptions.order = [['name', 'ASC']];
+            return findOptions
+
+        },
+        after: async (companies) => {
+            return companies
+        }
+    }),
 };
